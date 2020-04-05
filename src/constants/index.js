dbe783942eb3c07aa798d073c0002382e1cc3ae0
Promlert Lovichit <promlert@gmail.com>
2020-04-05 15:39:20 +0700

    เพิ่มเสียง

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,45 @@
 //export const TEST = 'test';
+export const JIGSAW_DATA = [
+    // ภาพ 1
+    {
+        board: {
+            image: require('../../assets/images/jigsaw/01/board.jpg')
+        },
+        sound: 'music.m4a',
+        pieces: [
+            {
+                image: require('../../assets/images/jigsaw/01/pieces/01.png'),
+                left: 0, top: 0,
+                visible: false,
+            },
+            {
+                image: require('../../assets/images/jigsaw/01/pieces/02.png'),
+                left: 308, top: 0,
+                visible: false,
+            },
+            {
+                image: require('../../assets/images/jigsaw/01/pieces/03.png'),
+                right: -2, top: 0,
+                visible: false,
+            },
+            {
+                image: require('../../assets/images/jigsaw/01/pieces/04.png'),
+                left: 0, bottom: 0,
+                visible: false,
+            },
+            {
+                image: require('../../assets/images/jigsaw/01/pieces/05.png'),
+                left: 205, bottom: 0,
+                visible: false,
+            },
+            {
+                image: require('../../assets/images/jigsaw/01/pieces/06.png'),
+                right: -1, bottom: 0,
+                visible: false,
+            },
+        ],
+    },
+
+    // ภาพ 2
+    // ภาพ 3
+];
--- src/screens/Game.js
+++ src/screens/Game.js
@@ -2,74 +2,90 @@ import React, {useState, useEffect} from 'react';
 import {
     SafeAreaView,
     StyleSheet,
-    ScrollView,
     View,
-    Text,
+    Button,
     Image,
     ImageBackground,
     StatusBar,
+    TouchableOpacity,
     Dimensions,
-    Button,
-    Alert,
 } from 'react-native';
 
-const imageBoard = require('../../assets/images/jigsaw/01/board.jpg');
-const pieces = [
-    {
-        image: require('../../assets/images/jigsaw/01/pieces/01.png'),
-        left: 0, top: 0,
-    },
-    {
-        image: require('../../assets/images/jigsaw/01/pieces/02.png'),
-        left: 308, top: 0,
-    },
-    {
-        image: require('../../assets/images/jigsaw/01/pieces/03.png'),
-        right: -2, top: 0,
-    },
-    {
-        image: require('../../assets/images/jigsaw/01/pieces/04.png'),
-        left: 0, bottom: 0,
-    },
-    {
-        image: require('../../assets/images/jigsaw/01/pieces/05.png'),
-        left: 205, bottom: 0,
-    },
-    {
-        image: require('../../assets/images/jigsaw/01/pieces/06.png'),
-        right: 0, bottom: 0,
-    },
-];
+import Sound from 'react-native-sound';
+// Enable playback in silence mode
+Sound.setCategory('Playback');
+
+import * as Constants from '../constants';
+
+const {width, height} = Dimensions.get('window');
+//alert(`Screen [width: ${width}, height: ${height}]`);
+const longestWidth = width > height ? width : height;
+const isLargeScreen = longestWidth > 900;
+const BOARD_MARGIN = isLargeScreen ? 100 : 40;
 
 function Game() {
+    const boardImage = Constants.JIGSAW_DATA[0].board.image;
+    const {sound} = Constants.JIGSAW_DATA[0];
+
     const [boardWidth, setBoardWidth] = useState(0);
     const [boardHeight, setBoardHeight] = useState(0);
     const [boardAndImageSourceRatio, setBoardAndImageSourceRatio] = useState(1.0);
+    const [pieces, setPieces] = useState(Constants.JIGSAW_DATA[0].pieces);
 
-    const {width: imageSourceWidth, height: imageSourceHeight} = Image.resolveAssetSource(imageBoard);
-
-    pieces.forEach(piece => {
+    pieces.forEach((piece, index) => {
         const {width, height} = Image.resolveAssetSource(piece.image);
         piece.width = width;
         piece.height = height;
+        //piece.zIndex = index;
     });
 
+    let music = null;
     useEffect(() => {
-    });
+        // Load the sound file from the app bundle
+        music = new Sound(sound, Sound.MAIN_BUNDLE, error => {
+            if (error) {
+                console.log('failed to load the sound', error);
+                return;
+            }
+            // loaded successfully
+            console.log('duration in seconds: ' + music.getDuration());
+
+            // Loop indefinitely until stop() is called
+            music.setNumberOfLoops(-1);
+
+            // Play the sound with an onEnd callback
+            music.play(success => {
+                if (success) {
+                    console.log('successfully finished playing');
+                } else {
+                    console.log('playback failed due to audio decoding errors');
+                }
+            });
+        });
+    }, []);
+    useEffect(() => () => {
+        if (music) {
+            // Stop the sound and rewind to the beginning
+            music.stop();
+            // Release the audio player resource
+            music.release();
+        }
+    }, []);
 
     const onLayout = (e) => {
         console.log(`Layout [width: ${e.nativeEvent.layout.width}, height: ${e.nativeEvent.layout.height}]`);
 
         const {width: layoutWidth, height: layoutHeight} = e.nativeEvent.layout;
         const layoutRatio = layoutWidth / layoutHeight;
+        const {width: imageSourceWidth, height: imageSourceHeight} = Image.resolveAssetSource(boardImage);
         const imageSourceRatio = imageSourceWidth / imageSourceHeight;
 
         let width, height;
         if (layoutRatio > imageSourceRatio) {
-            height = layoutHeight - 100;
+            height = layoutHeight - BOARD_MARGIN;
             width = (imageSourceWidth * height) / imageSourceHeight;
         } else {
-            width = layoutWidth - 100;
+            width = layoutWidth - BOARD_MARGIN;
             height = (imageSourceHeight * width) / imageSourceWidth;
         }
 
@@ -78,10 +94,26 @@ function Game() {
         setBoardAndImageSourceRatio(width / imageSourceWidth);
     };
 
+    const showPiece = (piece) => {
+        piece.visible = true;
+        /*const nextMaxZIndex = pieces.reduce((pieceMaxZIndex, piece) =>
+            ((pieceMaxZIndex.zIndex > piece.zIndex) ? pieceMaxZIndex : piece)
+        )[0].zIndex + 1;
+        piece.zIndex = nextMaxZIndex;*/
+        setPieces(pieces.slice());
+    };
+
     return (
         <View style={{flex: 1}}>
             <StatusBar barStyle="dark-content"/>
             <SafeAreaView style={{flex: 1}}>
+                <Button
+                    title={'CLEAR'}
+                    onPress={() => {
+                        pieces.forEach(piece => piece.visible = false);
+                        setPieces(pieces.slice());
+                    }}
+                />
                 <View
                     style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 0, borderWidth: 0, borderColor: 'red'}}
                     onLayout={onLayout}
@@ -91,37 +123,43 @@ function Game() {
                         style={{width: boardWidth, height: boardHeight, borderWidth: 0, borderColor: '#ccc'}}>
                         <ImageBackground
                             style={{justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'}}
-                            source={imageBoard}
+                            source={boardImage}
                             resizeMode={'contain'}
                         >
                             {pieces.map((piece, index) => {
-                                const style = {
-                                    width: piece.width * boardAndImageSourceRatio,
-                                    height: piece.height * boardAndImageSourceRatio,
+                                const {width, height} = piece;
+                                const pieceStyle = {
+                                    width: width * boardAndImageSourceRatio,
+                                    height: height * boardAndImageSourceRatio,
                                 };
+                                const touchableStyle = {};
+                                //touchableStyle.zIndex = piece.zIndex;
                                 if (piece.top != null) {
-                                    style.top = piece.top * boardAndImageSourceRatio;
+                                    touchableStyle.top = piece.top * boardAndImageSourceRatio;
                                 }
                                 if (piece.left != null) {
-                                    style.left = piece.left * boardAndImageSourceRatio;
+                                    touchableStyle.left = piece.left * boardAndImageSourceRatio;
                                 }
                                 if (piece.right != null) {
-                                    style.right = piece.right * boardAndImageSourceRatio;
+                                    touchableStyle.right = piece.right * boardAndImageSourceRatio;
                                 }
                                 if (piece.bottom != null) {
-                                    style.bottom = piece.bottom * boardAndImageSourceRatio;
+                                    touchableStyle.bottom = piece.bottom * boardAndImageSourceRatio;
                                 }
 
-                                if (index === 0 || index === 4) {
-                                    return (
+                                return (
+                                    <TouchableOpacity
+                                        key={index}
+                                        style={[styles.pieceTouchable, touchableStyle]}
+                                        activeOpacity={0.6}
+                                        onPress={() => showPiece(piece)}
+                                    >
                                         <Image
-                                            style={[styles.piece, style]}
-                                            source={piece.image}
+                                            style={pieceStyle}
+                                            source={piece.visible ? piece.image : null}
                                         />
-                                    );
-                                } else {
-                                    return (<View/>);
-                                }
+                                    </TouchableOpacity>
+                                );
                             })}
                         </ImageBackground>
                     </View>
@@ -133,11 +171,11 @@ function Game() {
 }
 
 const styles = StyleSheet.create({
-    piece: {
+    pieceTouchable: {
         position: 'absolute',
         borderWidth: 0,
         borderColor: 'red',
-    }
+    },
 });
 
 export default Game;
