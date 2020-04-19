ca13bf87f784c08b33eae9dc36e8c88dfaa620be
Promlert Lovichit <promlert@gmail.com>
2020-04-19 18:25:41 +0700

    multi-touch

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ export const JIGSAW_DATA = [
     // ภาพ 1
     {
         board: {
-            image: require('../../assets/images/jigsaw/01/board.jpg')
+            image: require('../../assets/images/jigsaw/01/board.jpg'),
         },
         sound: 'music.m4a',
         pieces: [
@@ -41,5 +41,108 @@ export const JIGSAW_DATA = [
     },
 
     // ภาพ 2
+    {},
+
     // ภาพ 3
+    {
+        board: {
+            image: require('../../assets/images/jigsaw/03/board.jpg'),
+        },
+        sound: 'music.m4a',
+        /*
+            (963 * x) / 1283
+            1: (963 * 582) / 1283 = 436.84
+            2: (963 * 462) / 1283 = 346.77
+            3: (963 * 583) / 1283 = 437.59
+            4: (963 * 444) / 1283 = 333.25
+            5: (963 * 738) / 1283 = 553.93
+            6: (963 * 445) / 1283 = 334.01
+            7: (963 * 582) / 1283 = 436.84
+            8: (963 * 462) / 1283 = 346.77
+            9: (963 * 583) / 1283 = 437.59
+        */
+        pieces: [
+            {
+                image: require('../../assets/images/jigsaw/03/pieces/01.png'),
+                left: 0, top: 0,
+                visible: false,
+                touches: [
+                    {x: 60, y: 130},
+                    {x: 300, y: 130},
+                ],
+            },
+            {
+                image: require('../../assets/images/jigsaw/03/pieces/02.png'),
+                left: 308, top: 0,
+                visible: false,
+                touches: [
+                    {x: 370, y: 210},
+                    {x: 590, y: 50},
+                ],
+            },
+            {
+                image: require('../../assets/images/jigsaw/03/pieces/03.png'),
+                right: 0, top: 0,
+                visible: false,
+                touches: [
+                    {x: 640, y: 130},
+                    {x: 910, y: 210},
+                ],
+            },
+            {
+                image: require('../../assets/images/jigsaw/03/pieces/04.png'),
+                left: 0, top: 163,
+                visible: false,
+                touches: [
+                    {x: 160, y: 280},
+                    {x: 160, y: 490},
+                ],
+            },
+            {
+                image: require('../../assets/images/jigsaw/03/pieces/05.png'),
+                left: 204, top: 246,
+                visible: false,
+                touches: [
+                    {x: 410, y: 385},
+                    {x: 560, y: 385},
+                ],
+            },
+            {
+                image: require('../../assets/images/jigsaw/03/pieces/06.png'),
+                right: 0, top: 163,
+                visible: false,
+                touches: [
+                    {x: 740, y: 310},
+                    {x: 870, y: 440},
+                ],
+            },
+            {
+                image: require('../../assets/images/jigsaw/03/pieces/07.png'),
+                left: 0, bottom: 2,
+                visible: false,
+                touches: [
+                    {x: 55, y: 550},
+                    {x: 255, y: 700},
+                ],
+            },
+            {
+                image: require('../../assets/images/jigsaw/03/pieces/08.png'),
+                left: 308, bottom: 2,
+                visible: false,
+                touches: [
+                    {x: 485, y: 550},
+                    {x: 485, y: 680},
+                ],
+            },
+            {
+                image: require('../../assets/images/jigsaw/03/pieces/09.png'),
+                right: 0, bottom: 2,
+                visible: false,
+                touches: [
+                    {x: 900, y: 580},
+                    {x: 700, y: 710},
+                ],
+            },
+        ],
+    },
 ];
--- src/screens/Game.js
+++ src/screens/Game.js
@@ -3,6 +3,7 @@ import {
     SafeAreaView,
     StyleSheet,
     View,
+    Text,
     Button,
     Image,
     ImageBackground,
@@ -11,6 +12,7 @@ import {
     Dimensions,
 } from 'react-native';
 
+import NfcManager, {NfcEvents} from 'react-native-nfc-manager';
 import Sound from 'react-native-sound';
 // Enable playback in silence mode
 Sound.setCategory('Playback');
@@ -22,15 +24,27 @@ const {width, height} = Dimensions.get('window');
 const longestWidth = width > height ? width : height;
 const isLargeScreen = longestWidth > 900;
 const BOARD_MARGIN = isLargeScreen ? 100 : 40;
+const TOUCH_POINT_SIZE = 26;
 
 function Game() {
-    const boardImage = Constants.JIGSAW_DATA[0].board.image;
-    const {sound} = Constants.JIGSAW_DATA[0];
+    const jigsawData = Constants.JIGSAW_DATA[2];
+
+    const touchPoints = [];
+    jigsawData.pieces.forEach(piece => {
+        piece.touches.forEach(touch => {
+            touchPoints.push(touch);
+        });
+    });
+    //console.log('TOUCH POINTS: ' + JSON.stringify(touchPoints));
+
+    const boardImage = jigsawData.board.image;
+    const {sound} = jigsawData;
 
     const [boardWidth, setBoardWidth] = useState(0);
     const [boardHeight, setBoardHeight] = useState(0);
     const [boardAndImageSourceRatio, setBoardAndImageSourceRatio] = useState(1.0);
-    const [pieces, setPieces] = useState(Constants.JIGSAW_DATA[0].pieces);
+    const [pieces, setPieces] = useState(jigsawData.pieces);
+    const [touches, setTouches] = useState([]);
 
     pieces.forEach((piece, index) => {
         const {width, height} = Image.resolveAssetSource(piece.image);
@@ -71,6 +85,46 @@ function Game() {
             music.release();
         }
     }, []);
+    /*useEffect(() => {
+        NfcManager.start();
+        NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
+            //console.warn('tag', tag);
+            //alert('พบ NFC Tag - ID: ' + tag.id);
+            //NfcManager.setAlertMessageIOS('I got your tag!');
+            //NfcManager.unregisterTagEvent().catch(() => 0);
+
+            if (tag.id === '451D5D9A') {
+                showPiece(pieces[0]);
+            } else if (tag.id === '05C1719B') {
+                showPiece(pieces[1]);
+            }
+        });
+
+        try {
+            NfcManager.registerTagEvent();
+        } catch (ex) {
+            console.warn('ex', ex);
+            NfcManager.unregisterTagEvent().catch(() => 0);
+        }
+
+        return () => {
+            NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+            NfcManager.unregisterTagEvent().catch(() => 0);
+        }
+    }, []);*/
+
+    const test = async () => {
+        try {
+            await NfcManager.registerTagEvent();
+        } catch (ex) {
+            console.warn('ex', ex);
+            NfcManager.unregisterTagEvent().catch(() => 0);
+        }
+    };
+
+    const cancelTest = () => {
+        NfcManager.unregisterTagEvent().catch(() => 0);
+    };
 
     const onLayout = (e) => {
         console.log(`Layout [width: ${e.nativeEvent.layout.width}, height: ${e.nativeEvent.layout.height}]`);
@@ -94,13 +148,49 @@ function Game() {
         setBoardAndImageSourceRatio(width / imageSourceWidth);
     };
 
-    const showPiece = (piece) => {
+    const showPiece = piece => {
         piece.visible = true;
         /*const nextMaxZIndex = pieces.reduce((pieceMaxZIndex, piece) =>
             ((pieceMaxZIndex.zIndex > piece.zIndex) ? pieceMaxZIndex : piece)
         )[0].zIndex + 1;
         piece.zIndex = nextMaxZIndex;*/
         setPieces(pieces.slice());
+    };
+
+    const handleTouch = e => {
+        const allTouches = e.nativeEvent.touches;
+        setTouches(allTouches);
+        console.log('START: ' + allTouches.length);
+        console.log(allTouches);
+
+        for (let i = 0; i < pieces.length; i++) {
+            let hit = true;
+            const piece = pieces[i];
+            for (let j = 0; j < piece.touches.length; j++) {
+                const touchPoint = piece.touches[j];
+                const numHit = allTouches.reduce((total, userTouch) => {
+                    if ((userTouch.locationX > (touchPoint.x * boardAndImageSourceRatio) - (TOUCH_POINT_SIZE / 2))
+                        && (userTouch.locationX < (touchPoint.x * boardAndImageSourceRatio) + (TOUCH_POINT_SIZE / 2))
+                        && (userTouch.locationY > (touchPoint.y * boardAndImageSourceRatio) - (TOUCH_POINT_SIZE / 2))
+                        && (userTouch.locationY < (touchPoint.y * boardAndImageSourceRatio) + (TOUCH_POINT_SIZE / 2))) {
+                        return total + 1;
+                    } else {
+                        return total;
+                    }
+                }, 0);
+
+                hit = hit && (numHit > 0);
+            }
+            if (hit) {
+                if (!piece.visible) {
+                    piece.visible = true;
+                    setPieces(pieces.slice());
+                }
+                break;
+            }
+        }
+
+        return false;
     };
 
     return (
@@ -114,6 +204,15 @@ function Game() {
                         setPieces(pieces.slice());
                     }}
                 />
+                {/*<View>
+                    <Text style={[styles.text, {textAlign: 'center', padding: 8}]}>{touches.length > 0 ? `มีการแตะภาพ ${touches.length} ตำแหน่ง` : 'ยังไม่มีการแตะภาพ'}</Text>
+                    {touches.map((touch, index) => (
+                        <Text
+                            key={index}
+                            style={[styles.text, {textAlign: 'center', padding: 4}]}
+                        >{`X: ${touch.locationX.toFixed(2)}, Y: ${touch.locationY.toFixed(2)}`}</Text>
+                    ))}
+                </View>*/}
                 <View
                     style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 0, borderWidth: 0, borderColor: 'red'}}
                     onLayout={onLayout}
@@ -147,8 +246,7 @@ function Game() {
                                     touchableStyle.bottom = piece.bottom * boardAndImageSourceRatio;
                                 }
 
-                                return (
-                                    <TouchableOpacity
+                                /*<TouchableOpacity
                                         key={index}
                                         style={[styles.pieceTouchable, touchableStyle]}
                                         activeOpacity={0.6}
@@ -158,13 +256,61 @@ function Game() {
                                             style={pieceStyle}
                                             source={piece.visible ? piece.image : null}
                                         />
-                                    </TouchableOpacity>
+                                    </TouchableOpacity>*/
+                                return (
+                                    <Image
+                                        key={index}
+                                        style={[styles.pieceTouchable, pieceStyle, touchableStyle]}
+                                        source={piece.visible ? piece.image : null}
+                                    />
                                 );
                             })}
+
+                            {touchPoints.map((point, index) => (
+                                <View
+                                    key={index}
+                                    style={{
+                                        position: 'absolute',
+                                        width: TOUCH_POINT_SIZE, height: TOUCH_POINT_SIZE,
+                                        left: (point.x * boardAndImageSourceRatio) - (TOUCH_POINT_SIZE / 2),
+                                        top: (point.y * boardAndImageSourceRatio) - (TOUCH_POINT_SIZE / 2),
+                                        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                                        borderRadius: TOUCH_POINT_SIZE / 2,
+                                    }}
+                                />
+                            ))}
+
+                            <View
+                                style={{
+                                    position: 'absolute',
+                                    width: '100%', height: '100%',
+                                    borderWidth: 1,
+                                    borderColor: '#aaa',
+                                }}
+                                onStartShouldSetResponder={handleTouch}
+
+                                onResponderGrant={e => {
+                                    //console.log('GRANT!');
+                                    /*const allTouches = e.nativeEvent.touches;
+                                    console.log(allTouches);*/
+                                }}
+                            />
                         </ImageBackground>
                     </View>
                     }
                 </View>
+                {/*<View
+                    style={{flexDirection: 'row', justifyContent: 'space-between'}}
+                >
+                    <Button
+                        title={'Test NFC'}
+                        onPress={test}
+                    />
+                    <Button
+                        title={'Cancel Test NFC'}
+                        onPress={cancelTest}
+                    />
+                </View>*/}
             </SafeAreaView>
         </View>
     );
@@ -175,6 +321,10 @@ const styles = StyleSheet.create({
         position: 'absolute',
         borderWidth: 0,
         borderColor: 'red',
+    },
+    text: {
+        color: '#666',
+        fontSize: 18,
     },
 });
 
